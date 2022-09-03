598630b93d48a20a2bcc8adbda5e15b7d5f16bb0
siddheshkotkar <kotkar.sidh@gmail.com>
2022-09-03 17:46:15 +0530

    Fixed some bugs

---
+++ src/elecbill/Conn.java
@@ -0,0 +1,19 @@
+package elecbill;
+
+import java.sql.*;  
+
+public class Conn{
+    Connection c;
+    Statement s;
+    public Conn(){  
+        try{  
+            Class.forName("com.mysql.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql:///ebs","root","swami@sid");    
+            s =c.createStatement();  
+            
+           
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+} 
